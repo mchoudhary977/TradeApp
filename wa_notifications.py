e84d404ea1de0bef4c6b6e22e5d419542bd656b9
Mukesh <mchoudhary977@gmail.com>
2023-08-17 14:19:00 +0530

    whatsapp notification

--- wa_notifications.py
+++ wa_notifications.py
@@ -10,49 +10,47 @@ def send_whatsapp_msg(mtitle='TRADE-APP', mtext='Welcome to TradeApp!'):
         'Authorization': tkn,
         'Content-Type': 'application/json'
     }
-    # payload = {"messaging_product": "whatsapp", "to": "919673843177", "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }
-
-
-    payload = {
-        "messaging_product":"whatsapp",
-        "recipient_type":"individual",
-        "to":"919673843177",
-        "type":"template",
-        "template":{
-            "name":"app_msg",
-            "language":{
-                "code":"en"
-                },
-            "components":[
-                {
-                    "type":"header",
-                    "parameters":[
-                        {
-                            "type":"text",
-                            "text":mtitle
-                            }
-                        ]
+    phone_list = ['919673843177','917721841919']
+    # payload = {"messaging_product"1: "whatsapp", "to": "919673843177", "type": "template", "template": { "name": "hello_world", "language": { "code": "en_US" } } }
+    
+    for i in phone_list:
+        payload = {
+            "messaging_product":"whatsapp",
+            "recipient_type":"individual",
+            "to":i,
+            "type":"template",
+            "template":{
+                "name":"app_msg",
+                "language":{
+                    "code":"en"
                     },
-                {
-                    "type":"body",
-                    "parameters":[
-                        {
-                            "type":"text",
-                            "text":mtext
-                            }
-                        ]
-                    }
-                ]
+                "components":[
+                    {
+                        "type":"header",
+                        "parameters":[
+                            {
+                                "type":"text",
+                                "text":mtitle
+                                }
+                            ]
+                        },
+                    {
+                        "type":"body",
+                        "parameters":[
+                            {
+                                "type":"text",
+                                "text":mtext
+                                }
+                            ]
+                        }
+                    ]
+                }
             }
-        }
-
-    # Send the POST request
-    response = requests.post(url, headers=headers, json=payload)
-
-    # Check the response
-    if response.status_code == 200:
-        # return render_template('index.html',alert_msg = 'WhatsApp Alert Trigerred')
-        return {'status':'SUCCESS','msg':response.json()}
-    else:
-        # return render_template('index.html',alert_msg = response.text)
-        return {'status':'ERROR','msg':response.text}
+    
+        # Send the POST request
+        response = requests.post(url, headers=headers, json=payload)
+    
+        # Check the response
+        if response.status_code != 200:
+            return {'status':'ERROR','msg':f"{i} - {response.text}"}
+    return {'status':'SUCCESS','msg':f"{response.json()}"}
