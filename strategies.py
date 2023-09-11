38fddc784f7ba78a27a18392773ec8528344b970
Mukesh <mchoudhary977@gmail.com>
2023-09-11 08:33:37 +0530

    strategies update for option number

--- strategies.py
+++ strategies.py
@@ -288,7 +288,8 @@ def check_ema_signal(ticker, ema_sig, last_px):
         ema_sig['active']='N'
         # count changed to 4 due to balance constraints
         opt = ic_option_chain(ticker, underlying_price=last_px, option_type=opt_type, duration=0)
-        opt = opt.iloc[4]
+        num = json.load(open('config.json', 'r'))[ticker]['OPT#']
+        opt = opt.iloc[num]
         sec_id = opt['TK']
         sec_name = opt['CD']
         exit_msg = ''
